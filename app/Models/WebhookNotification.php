e20f69097470c68c2020531c91d2f628fe486217
yemiwebby <yemiwebby@gmail.com>
2021-11-25 06:02:57 +0100

    Added fillable properties

--- app/Models/WebhookNotification.php
+++ app/Models/WebhookNotification.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class WebhookNotification extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'notification_id', 'type', 'happened_at', 'has_vcs_info', 'commit_subject','commit_author',
+        'event_status','workflow_url'
+    ];
 }
